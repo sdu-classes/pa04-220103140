f2add1eb0203a9602230da157313451dd3ca4a09
mukhamedjan140 <124435839+220103140@users.noreply.github.com>
2023-02-24 19:49:58 +0600

    Update and rename Rabbit.java to Cat.java

---
+++ problem-B/Cat.java
@@ -0,0 +1,18 @@
+public class Cat extends Animal implements Pet {    private String catName;
+    public Cat(){        this("");
+    }
+    public Cat(String name) {
+        super(4);        catName = name;
+    }
+    @Override    public String getName() {
+       return catName;    }
+    @Override
+    public void setName(String name) {        String setName = name;
+    }
+    @Override
+    public void play() {        System.out.println("Cat play");
+    }
+    @Override
+    public void eat() {        System.out.println("Cat eat ");
+    }
+}
