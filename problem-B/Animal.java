b7188e5d28a902ec3f56b355afb4089c82733c1f
mukhamedjan140 <124435839+220103140@users.noreply.github.com>
2023-02-24 19:43:04 +0600

    Update Animal.java

---
+++ problem-B/Animal.java
@@ -0,0 +1,8 @@
+public abstract class Animal {    protected int legs;
+    protected Animal(int legs){      this.legs = legs ;
+    }
+
+    public void walk(){        System.out.println("walk by " + legs+" legs");
+    }    public void eat(){
+    }
+}
