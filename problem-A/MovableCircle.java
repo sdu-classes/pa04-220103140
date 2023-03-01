646a0be448e0273178c69f5859dacb47039d2d5f
mukhamedjan140 <124435839+220103140@users.noreply.github.com>
2023-03-01 14:24:11 +0600

    Update MovableCircle.java

--- problem-A/MovableCircle.java
+++ problem-A/MovableCircle.java
@@ -1,33 +1,28 @@
 public class MovableCircle implements Movable{
     private int radius;
+    private MovablePoint center;
 
-    public MovaeblePoint center;
-
-    public MoveableCircle(int x , int y ,int xSpeed , int ySpeed,int radius){
-        center = new MovaeblePoint(x,y,xSpeed,ySpeed);
-        this.radius=radius;
-    }
-
-    @Override
-    public void moveUp() {
-        center.moveUp();
-    }
-
-    @Override
-    public void moveDown() {
-        center.moveDown();
-    }
-
-    @Override
-    public void moveLeft() {
-        center.moveLeft();
-    }
-
-    @Override
-    public void moveRight() {
-        center.moveRight();
+    public MovableCircle(int x, int y, int xSpeed, int ySpeed, int radius){
+        this.center = new MovablePoint(x, y, xSpeed, ySpeed);
+        this.radius = radius;
     }
     public String toString(){
-        return "(" + center.x + "," + center.y + "), speed =(" + center.xSpeed + "," + center.ySpeed + "), radius = " + radius;
-}
+        return this.center.toString() + ",radius=" + this.radius;
+    }
+    @Override
+    public void moveUp(){
+        this.center.moveUp();
+    }
+    @Override
+    public void moveDown(){
+        this.center.moveDown();
+    }
+    @Override
+    public void moveLeft(){
+        this.center.moveLeft();
+    }
+    @Override
+    public void moveRight(){
+        this.center.moveRight();
+    }
 }
